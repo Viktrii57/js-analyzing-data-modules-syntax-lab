6f0f47dd23ae8ab5035976c4f2e6cb409d7996a9
Viktrii57 <benvictor.gecure57@gmail.com>
2026-04-12 18:51:31 +0300

    Completed modules and syntax lab and pass all tests

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
+const { format } = require('date-fns');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (let arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  const today = new Date();
+  combinedObject.merge_date = format(today, '4/12/2026');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
